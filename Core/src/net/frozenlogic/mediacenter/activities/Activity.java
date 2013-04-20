86468da3dfe064b2a9e0ca9188bf95e0583d37c7
crypticman <alex.d.ivan@gmail.com>
2013-04-20 20:03:16 +0300

    PicasaPhotosPlugin Libs

--- Core/src/net/frozenlogic/mediacenter/activities/Activity.java
+++ Core/src/net/frozenlogic/mediacenter/activities/Activity.java
@@ -1,10 +1,12 @@
 package net.frozenlogic.mediacenter.activities;
 
+import net.frozenlogic.mediacenter.impl.plugins.picasa.PicasaPhotosClientException;
+
 public interface Activity {
 
     void initialize(ActivityContext activityContext);
 
-    Activity perform(ActivityContext activityContext);
+    Activity perform(ActivityContext activityContext) throws PicasaPhotosClientException;
 
     ActivityType getActivityType();
 }
